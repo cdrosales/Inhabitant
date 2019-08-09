387a6326ec6225699394e4b16d157374105554c6
cdrosales <cara10denise@hotmail.com>
2019-08-09 14:43:32 -0400

    feat: added footer info

--- themes/inhabitantTheme/footer.php
+++ themes/inhabitantTheme/footer.php
@@ -1,4 +1,34 @@
-<?php wp_footer(); ?>  <!-- will give admin pannel & script tags will be loaded here  -->
+<?php wp_footer(); 
+
+
+
+?>  <!-- will give admin pannel & script tags will be loaded here  -->
+
+
+
+<ul class="footerInhabitantInfo">
+    <li>Contact Info</li>
+    <li>Info@inhabitant.com</li>
+    <li>778-456-7891</li>
+    <li> 
+        <ul class="socialMediaIcons">
+            <li><i class="fab fa-facebook-square"></i></li>
+            <li><i class="fab fa-twitter-square"></i></li>
+            <li><i class="fab fa-google-plus-square"></i></li>
+        </ul>
+    </li>
+</ul>
+
+<ul class="footerHours">
+    <li>Business Hours</li>
+    <li>Monday-Friday: 9am to 5pm</li>
+    <li>Saturday: 10am to 2pm</li>
+    <li>Sunday: Closed</li>
+</ul>
+
+<p>Copyright © 2019 Inhabitant</p>
+
+
 
 </body>
 </html>
--- themes/inhabitantTheme/page-about.php
+++ themes/inhabitantTheme/page-about.php
@@ -1,24 +1,29 @@
 <?php get_header(); ?> 
 
-<h1><?php bloginfo('name'); ?></h1>
+<!-- <h1><?php bloginfo('name'); ?></h1> -->
 
     
-<?php if( have_posts() ): 
+<!-- <?php if( have_posts() ): 
 echo '<h1>PAGE-ABOUT.PHP</h1>';
 
 
     while ( have_posts() ): 
-        the_post();?> 
+        the_post();?>  -->
 
-    <h2><?php the_title(); ?></h2> 
-    <?php the_content(); ?>
+    <div class ="aboutTitle">
+        <h2><?php the_title(); ?></h2> 
+    </div>
+
+    <div class="about Content">
+        <?php the_content(); ?>
+    </div>
 
 
-    <?php endwhile; ?> 
+    <!-- <?php endwhile; ?>  -->
 
-    <?php the_posts_navigation(); ?> 
+    <!-- <?php the_posts_navigation(); ?> 
 <?php else : ?>
         <p>No posts found</p>
-<?php endif; ?>
+<?php endif; ?> -->
 
 <?php get_footer(); ?>
