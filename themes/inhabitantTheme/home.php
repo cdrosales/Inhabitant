51fd1b235be689bbdfd0c5053223a9589e500613
cdrosales <cara10denise@hotmail.com>
2019-08-12 14:19:58 -0400

    feat: added sidebar with styles

--- themes/inhabitantTheme/home.php
+++ themes/inhabitantTheme/home.php
@@ -1,39 +1,56 @@
-<?php get_header(); ?> 
+<div class="gridContainer">
 
-<!-- <h1><?php bloginfo('name'); ?></h1> -->
+    <header>
+    <?php get_header(); ?> 
+    </header>
+
+    <!-- <h1><?php bloginfo('name'); ?></h1> -->
+
+
+    <div class="sidebar">
+    <?php dynamic_sidebar('sidebar-1'); ?>
+    </div>
 
 
 
-<?php if( have_posts() ): 
 
-    while ( have_posts() ):   //runs the loop
-        the_post();?>   <!-- grab info to post -->
-    
+    <main>
+    <?php if( have_posts() ): 
 
-    <div class="journalTitle">
-        <h2><?php the_title(); ?></h2> 
+        while ( have_posts() ):   //runs the loop
+            the_post();?>   <!-- grab info to post -->
+        
 
-        <div class="postData">
-            <p>
-                <span class="postDate"><?php echo date("d F Y"); ?></span>
-                /
-                <span class="postComments"><?php echo get_comments(); ?></span>        
-                / by
-                <span class="postAuthor"><?php echo get_the_author(); ?></span>
-            </p>
+        <div class="journalTitle">
+            <h2><?php the_title(); ?></h2> 
+
+            <div class="postData">
+                <p>
+                    <span class="postDate"><?php echo date("d F Y"); ?></span>
+                    /
+                    <span class="postComments"><?php echo get_comments(); ?></span>        
+                    / by
+                    <span class="postAuthor"><?php echo get_the_author(); ?></span>
+                </p>
+            </div>
         </div>
-    </div>
-    
-    <div class="journalContent">
-        <?php the_content(); ?> 
-    </div>
+        
+        <div class="journalContent">
+            <?php the_content(); ?> 
+        </div>
 
 
-    <?php endwhile; ?>   <!-- loop ends -->
 
-    <?php the_posts_navigation(); ?> 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif; ?>
+        <?php endwhile; ?>   <!-- loop ends -->
 
-<?php get_footer(); ?>
+        <?php the_posts_navigation(); ?> 
+        <?php else : ?>
+            <p>No posts found</p>
+    <?php endif; ?>
+    </main>
+
+    <footer>
+    <?php get_footer(); ?>
+        </footer>
+
+</div>
