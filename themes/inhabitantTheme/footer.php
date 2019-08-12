6be3e8b01eecd1b0376ef0f812e63afd40732535
cdrosales <cara10denise@hotmail.com>
2019-08-11 20:52:15 -0400

    feat:added logo to footer section

--- themes/inhabitantTheme/footer.php
+++ themes/inhabitantTheme/footer.php
@@ -3,28 +3,40 @@
 ?>  <!-- will give admin pannel & script tags will be loaded here  -->
 
 <footer>
-    <div class="footerMainContainer">
-        <ul class="footerInhabitantInfo">
-            <li><h3>Contact Info</h3></li>
-            <li><i class="fas fa-envelope"></i>Info@inhabitant.com</li>
-            <li><i class="fas fa-phone-alt"></i>778-456-7891</li>
-            <li> 
-                <ul class="socialMediaIcons">
-                    <li><i class="fab fa-facebook-square"></i></li>
-                    <li><i class="fab fa-twitter-square"></i></li>
-                    <li><i class="fab fa-google-plus-square"></i></li>
-                </ul>
-            </li>
-        </ul>
 
-        <ul class="footerHours">
-            <li><h3>Business Hours</h3></li>
-            <li>Monday-Friday: 9am to 5pm</li>
-            <li>Saturday: 10am to 2pm</li>
-            <li>Sunday: Closed</li>
-        </ul>
+    <div class="footerMainContainer">
+        <div class="footerContainer">
+
+            <ul class="footerInhabitantInfo">
+                <li><h3>Contact Info</h3></li>
+                <li><i class="fas fa-envelope"></i>Info@inhabitant.com</li>
+                <li><i class="fas fa-phone-alt"></i>778-456-7891</li>
+                <li> 
+                    <ul class="socialMediaIcons">
+                        <li><i class="fab fa-facebook-square"></i></li>
+                        <li><i class="fab fa-twitter-square"></i></li>
+                        <li><i class="fab fa-google-plus-square"></i></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="footerHours">
+                <li><h3>Business Hours</h3></li>
+                <li>Monday-Friday: 9am to 5pm</li>
+                <li>Saturday: 10am to 2pm</li>
+                <li>Sunday: Closed</li>
+            </ul>
+            
+        </div>
+
+
+             <img src="<?php echo get_template_directory_uri() . '/assets/images/logos/inhabitent-logo-text.svg'; ?>" class="footerLogo" />
+
     </div>
-    
+
+
+
+
     <p class="copyright">Copyright © 2019 Inhabitant</p>
 
 </footer>
