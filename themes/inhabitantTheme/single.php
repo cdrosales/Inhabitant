2d46cdbf90fbbc5dc55db978f01f40fd11a62ee3
cdrosales <cara10denise@hotmail.com>
2019-08-12 20:17:08 -0400

    style: added styles to single journal posts

--- themes/inhabitantTheme/single.php
+++ themes/inhabitantTheme/single.php
@@ -1,25 +1,54 @@
-<?php get_header(); ?> <!-- dont need include syntax for wordpress -->
+<?php get_header(); ?> 
 
-<h1><?php bloginfo('name'); ?></h1>
+    <!-- <h1><?php bloginfo('name'); ?></h1> -->
 
-<!-- locate blog post and grab few key points via THE LOOP-->
-<!-- the while loop is the MAIN component -->
     
-<?php if( have_posts() ): // have posts - checks if theres anypost available in your feed
-// The WordPress Loop: loads post content
-    while ( have_posts() ): // run as many times as many blog posts available?***
-        the_post();?> <!--singular not multiple -- THIS LOADS POST CONTENT***-->
+    <section>
 
-    <h2><?php the_title(); ?></h2> <!--customizable to what you wanna grab ex URL can be grabbbed-->
-    <?php the_content(); ?>
+        <main>
+            <?php if( have_posts() ): 
+
+            while ( have_posts() ):   //runs the loop
+                the_post();?>   <!-- grab info to post -->
+            
+
+            <div class="journalTitle">
+                <h2><?php the_title(); ?></h2> 
+
+                <div class="postData">
+                    <p>
+                        <span class="postDate"><?php echo date("d F Y"); ?></span>
+                        /
+                        <span class="postComments"><?php echo get_comments(); ?></span>        
+                        / by
+                        <span class="postAuthor"><?php echo get_the_author(); ?></span>
+                    </p>
+                </div>
+            </div>
+            
+            <div class="journalContent">
+                <?php the_content(); ?> 
+            </div>
 
 
-    <?php endwhile; ?> <!--Loop ENDS-->
 
-    <?php the_posts_navigation(); ?> <!--only if we have way more content than shown on page-->
+            <?php endwhile; ?>   <!-- loop ends -->
+        </main>
 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif; ?>
+
+        <div class="sidebarContent">
+            <div class="sidebar">
+            <?php dynamic_sidebar('sidebar-1'); ?>
+            </div>
+        </div>
+    </section>    
+
+
+        <?php the_posts_navigation(); ?> 
+        <?php else : ?>
+            <p>No posts found</p>
+    <?php endif; ?>
+
+    
 
 <?php get_footer(); ?>
