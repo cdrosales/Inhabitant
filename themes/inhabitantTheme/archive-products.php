b8f10429c45b9dd505f2ceda7ec352bf1dbdf36f
cdrosales <cara10denise@hotmail.com>
2019-08-13 00:43:35 -0400

    style: added grid to products page

--- themes/inhabitantTheme/archive-products.php
+++ themes/inhabitantTheme/archive-products.php
@@ -4,15 +4,15 @@
 
 
 
-
+<div class="productsContainer">
 <?php if( have_posts() ): // have posts - checks if theres anypost available in your feed
 
 
     while ( have_posts() ): // run as many times as many blog posts available?***
         the_post();?> <!--singular not multiple -- THIS LOADS POST CONTENT***-->
 
-<div class="productsContainer">
 
+    <div class="products">
     <div class="productsThumbnail">
       <?php the_post_thumbnail('medium'); ?>
     </div>
@@ -20,11 +20,13 @@
     <div class="productsInfo">
         <?php echo the_title() . '............$' . get_field('price'); ?> 
     </div>
+    </div>
     
-</div>
+
 
 
     <?php endwhile; ?> <!--Loop ENDS-->
+</div>
 
 <?php else : ?>
         <p>No posts found</p>
