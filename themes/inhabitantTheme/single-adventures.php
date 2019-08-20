5f55245d0a46668eeec41999963dc48091732ddd
cdrosales <cara10denise@hotmail.com>
2019-08-20 14:29:01 -0400

    style: add styles to single adventure posts

--- themes/inhabitantTheme/single-adventures.php
+++ themes/inhabitantTheme/single-adventures.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?> 
 
-<!-- <h1><?php bloginfo('name'); ?></h1> -->
 
     
 <?php if( have_posts() ): 
@@ -8,30 +7,43 @@
         the_post();?>  
 
    
-    
+ 
 
 <div class="aboutContent">
 
     <div class="heroWrapper">
-        <div class ="aboutTitle">
-        <?php the_title(); ?> 
-        </div>
+        
         <div class="heroImage">
         <?php the_post_thumbnail(); ?>
         </div>
-        
-
     </div>
 
+    <div class="adventureTitle">
+        <?php the_title(); ?> 
+        </div>
+        <div class="adventureAuthor"><?php echo 'by ' . get_the_author(); ?></div>
+
+        <div class="adventurePost">    
         <?php the_content(); ?>
+    </div>
 </div>
 
 
     <?php endwhile; ?> 
 
+    
+
     <?php the_posts_navigation(); ?> 
 <?php else : ?>
         <p>No posts found</p>
 <?php endif; ?>
+
+<div class="adventureBtn">
+<div class="postBtn">
+    <button class="like button"><a href="http://www.facebook.com"></a> <i class="fab fa-facebook-f"></i>Like </button>
+    <button class="tweet button"><a href="http://www.twitter.com"></a> <i class="fab fa-twitter"></i>Tweet</button>
+    <button class="pin button"><a href="http://www.pinterest.com"></a> <i class="fab fa-pinterest"></i>Pin</button>
+</div>
+</div>
 
 <?php get_footer(); ?>
